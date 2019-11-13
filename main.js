35325a2db1df3e9bf646b0d9935771a69204c0d5
nenadvuk <nenadvukasinovic.rs@gmail.com>
2019-11-13 11:21:44 +0100

    let

--- main.js
+++ main.js
@@ -39,11 +39,11 @@ document.querySelector("#play").addEventListener("click", function () {
 
 
 // Numbers
-var numbers = [];
-for (var i = 0; i < 7; i++) {
-	var add = true;
-	var randomNumber = Math.floor(Math.random() * 39) + 1;
-	for (var y = 0; y < 39; y++) {
+let numbers = [];
+for (let i = 0; i < 7; i++) {
+	let add = true;
+	const randomNumber = Math.floor(Math.random() * 39) + 1;
+	for (let y = 0; y < 39; y++) {
 		if (numbers[y] == randomNumber) {
 			add = false;
 		}
@@ -55,9 +55,9 @@ for (var i = 0; i < 7; i++) {
 	}
 }
 
-var highestNumber = 0;
-for (var m = 0; m < numbers.length; m++) {
-	for (var n = m + 1; n < numbers.length; n++) {
+let highestNumber = 0;
+for (let m = 0; m < numbers.length; m++) {
+	for (let n = m + 1; n < numbers.length; n++) {
 		if (numbers[n] < numbers[m]) {
 			highestNumber = numbers[m];
 			numbers[m] = numbers[n];
@@ -69,7 +69,7 @@ for (var m = 0; m < numbers.length; m++) {
 
 
 // Displaying numbers within balls
-for (i = 1; i <= 7; i++) {
+for (let i = 1; i <= 7; i++) {
 	if (numbers[i - 1] < 10) {
 		document.getElementById(`res${i}`).innerHTML = `0${numbers[i - 1]}`;
 	} else document.getElementById(`res${i}`).innerHTML = numbers[i - 1];
@@ -77,16 +77,16 @@ for (i = 1; i <= 7; i++) {
 
 // Ticket
 // Chosing all elements with class td
-var checkedFields = document.querySelectorAll("td");
-var checkedNumbers = 0;
-var chosenNumbers = [];
-var sortedChosenNumbers = [];
-var chosenSevenOrNull = false;
-var count = document.getElementById("count");
-var chkdNumColor = document.querySelector(".checked-numbers");
-var play = document.getElementById("play")
-var gamesNumber = 0;
-for (var i = 0; i < checkedFields.length; i++) {
+let checkedFields = document.querySelectorAll("td");
+let checkedNumbers = 0;
+let chosenNumbers = [];
+let sortedChosenNumbers = [];
+let chosenSevenOrNull = false;
+let count = document.getElementById("count");
+let chkdNumColor = document.querySelector(".checked-numbers");
+let play = document.getElementById("play")
+let gamesNumber = 0;
+for (let i = 0; i < checkedFields.length; i++) {
 	checkedFields[i].addEventListener("mousedown", function () {
 		if (!chosenSevenOrNull) {
 			// Adding color to clicked field
@@ -118,7 +118,7 @@ for (var i = 0; i < checkedFields.length; i++) {
 
 // Clear last filed
 
-var clear = document.getElementById("clear");
+let clear = document.getElementById("clear");
 clear.addEventListener("click", function () {
 	chosenSevenOrNull = false;
 	chkdNumColor.style.backgroundColor = "white";
@@ -152,8 +152,8 @@ console.log(numbers)
 function compareArrays(num, sor) {
 
 	index = 0;
-	for (i = 0; i < num.length; i++) {
-		for (j = 0; j < sor.length; j++) {
+	for (let i = 0; i < num.length; i++) {
+		for (let j = 0; j < sor.length; j++) {
 			if (num[i] === sor[j]) {
 				index++
 			}
